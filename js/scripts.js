b3e04d4e9c97b2dbb3e02bb225bc4da2498f7d22
dvictoria2020 <vicdelfer@gmail.com>
2021-07-01 22:37:12 -0600

    commit inicial

--- js/scripts.js
+++ js/scripts.js
@@ -42,8 +42,73 @@ control_capas = L.control.layers(capas_base).addTo(mapa);
 // Control de escala
 L.control.scale().addTo(mapa);
 
+// Agregar capa WMS Asentamientos Informales MIVAH
+var capa_asentamientos_informales = L.tileLayer.wms('https://sig.mivah.go.cr/server/services/cit/AsentamientosInformales/MapServer/WmsServer?', {
+  layers: 'AsentamientosInformales',
+  format: 'image/png',
+  transparent: true,
+  opacity: 0.4
+}).addTo(mapa);
+
+// Se agrega al control de capas como de tipo "overlay"
+control_capas.addOverlay(capa_asentamientos_informales, 'Asentamientos Informales de Costa Rica WMS');
+
+// Capa raster deficit habitacional MIVAH
+var capa_deficit_habitacional = L.imageOverlay("https://dvictoria2020.github.io/proyecto/datos/DeficitHabitacional.png", 
+	[[9.9055601491738656, -84.1796468648582987], 
+	[9.9718107353507541, -84.0860167000804068]], 
+	{opacity:0.4}
+).addTo(mapa);
+control_capas.addOverlay(capa_deficit_habitacional, 'Déficit habitacional');
+
+function updateOpacityDef() {
+  document.getElementById("span-opacity-Def").innerHTML = document.getElementById("sld-opacity-Def").value;
+  capa_deficit_habitacional.setOpacity(document.getElementById("sld-opacity-Def").value);
+}
+
+// Capa de coropletas sobre terrenos del estado en formato GeoJSON
+$.getJSON('https://dvictoria2020.github.io/proyecto/datos/terrenos_estado.geojson', function (geojson) {
+  var capa_terrenos = L.choropleth(geojson, {
+	    valueProperty: 'area',
+	    scale: ['white', 'blue'],
+	    steps: 7,
+	    mode: 'q',
+	    style: {
+	      color: '#fff',
+	      weight: 0.5,
+	      fillOpacity: 0.5
+	    }, 
+      onEachFeature: function(feature, layer) {
+        var popupText = "<strong>Propietario</strong>: " + feature.properties.nom_juridi + "<br>" + "<strong>Tipo de Inmueble</strong>: " + feature.properties.t_inmueb +  "<br>" + "<strong>Amenazas</strong>: " + feature.properties.r_fisica + "<br>" + "<strong>Número de finca</strong>: " + feature.properties.finca;
+        layer.bindPopup(popupText);
+      }
+  }).addTo(mapa);
+  control_capas.addOverlay(capa_terrenos, 'Terrenos del Estado');
+
+  // Leyenda de la capa de coropletas sobre terrenos del estado
+  var leyenda = L.control({ position: 'bottomleft' })
+  leyenda.onAdd = function (mapa) {
+    var div = L.DomUtil.create('div', 'info legend')
+    var limits = capa_terrenos.options.limits
+    var colors = capa_terrenos.options.colors
+    var labels = []
+
+    // Add min & max
+    div.innerHTML = '<div class="labels"><div class="min">' + limits[0] + '</div> \
+            <div class="max">' + limits[limits.length - 1] + '</div></div>'
+
+    limits.forEach(function (limit, index) {
+      labels.push('<li style="background-color: ' + colors[index] + '"></li>')
+    })
+
+    div.innerHTML += '<ul>' + labels.join('') + '</ul>'
+    return div
+    }
+    leyenda.addTo(mapa)
+});
+
 // Capa vectorial de centroides asentamientos informales en formato GeoJSON
-$.getJSON("https://dvictoria2020.github.io/tarea-4/datos/asentamientos_informales.geojson", function(geodata) {
+$.getJSON("https://dvictoria2020.github.io/proyecto/datos/asentamientos_informales.geojson", function(geodata) {
   //Registros individuales
   var capa_asentamientos_informales = L.geoJson(geodata, {
     style: function() {
@@ -68,32 +133,4 @@ $.getJSON("https://dvictoria2020.github.io/tarea-4/datos/asentamientos_informale
   asentamientos_informales_agrupados.addTo(mapa);
   control_capas.addOverlay(asentamientos_informales_agrupados, 'Registros agrupados de asentamientos informales agrupados');
   control_capas.addOverlay(capa_asentamientos_informales, 'Registros individuales de asentamientos informales individuales');
-});
-
-// Capa vectorial de registros áreas públicas
-$.getJSON("https://dvictoria2020.github.io/tarea-4/datos/areas_publicas.geojson", function(geodata) {
-  //Registros individuales
-  var capa_areas_publicas = L.geoJson(geodata, {
-    style: function() {
-	  return {'color': "green", 'weight': 0.5}
-    },
-    onEachFeature: function(feature, layer) {
-      var popupText = "<strong>Nombre</strong>: " + feature.properties.nom_refere + "<br>" +
-                      "<strong>Tipo</strong>: " + feature.properties.tipo;
-      layer.bindPopup(popupText);
-    },			
-    pointToLayer: function(getJsonPoint, latlng) {
-        return L.marker(latlng, {icon: iconoParques});
-    }
-  });
-
-  // Capa de calor (heatmap)
-  coordenadas = geodata.features.map(feat => feat.geometry.coordinates.reverse());
-  var capa_areas_publicas_calor = L.heatLayer(coordenadas, {radius: 30, blur: 10, gradient: {0.5: 'purple', 0.8: 'green', 1: 'red'}});  
-  
-  // Se añaden las capas al mapa y al control de capas
-  capa_areas_publicas_calor.addTo(mapa);
-  control_capas.addOverlay(capa_areas_publicas_calor, 'Mapa de calor de las áreas públicas');
-  // capa_areas_publicas.addTo(mapa);
-  control_capas.addOverlay(capa_areas_publicas, 'Registros individuales de áreas públicas');
 });
